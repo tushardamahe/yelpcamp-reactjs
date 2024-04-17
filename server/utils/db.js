34032b8db4a6d2fb23317b133a9111cef554b24d
Tushar Damahe <qubetushar@gmail.com>
2024-04-17 23:49:07 +0530

    added models and connect to MongoDB

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const URI = "mongodb://127.0.0.1:27017/react-yelpcamp";
+const URI = process.env.MONGODB_URI;
 
 const connectDb = async () => {
   try {
