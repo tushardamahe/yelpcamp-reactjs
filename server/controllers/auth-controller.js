077312a69d1160b05fabf9d51745c76c8ba97aee
Tushar Damahe <qubetushar@gmail.com>
2024-04-18 23:07:44 +0530

    stored data on mongodb and use bcrypt to hash password

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("Welcome to Home");
@@ -8,8 +10,22 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    res.status(200).json("Welcome to Register from AC");
+    const { username, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "Email already exist" });
+    }
+
+    const userCreated = await User.create({
+      username,
+      email,
+      password,
+    });
+    res.status(200).json({ userCreated });
   } catch (error) {
+    console.log(error);
     res.status(400).json({ msg: "Page Not Found" });
   }
 };
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -15,6 +16,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    next();
+  }
+
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, saltRound);
+    user.password = hashPassword;
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = new mongoose.model("User", userSchema);
