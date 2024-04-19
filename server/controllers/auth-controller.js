10a8d598f4d7c0596d283e8e182a09baa4a5496f
Tushar Damahe <qubetushar@gmail.com>
2024-04-19 23:15:04 +0530

    added login route and login functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
   try {
@@ -34,4 +35,30 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { home, register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const user = await bcrypt.compare(password, userExist.password);
+
+    if (user) {
+      res.status(200).json({
+        msg: "Login Successful",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Email and Password" });
+    }
+  } catch (error) {
+    res.status(500).json("Internal Server Error");
+  }
+};
+
+module.exports = { home, register, login };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 
 router.route("/").get(authcontrollers.home);
-
 router.route("/register").post(authcontrollers.register);
+router.route("/login").post(authcontrollers.login);
 
 module.exports = router;
