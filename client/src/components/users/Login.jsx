306e061dcf43b390d46f201f234eb59d95d00c14
Tushar Damahe <qubetushar@gmail.com>
2024-04-28 23:01:35 +0530

    storing data in states in login form

--- client/src/components/users/Login.jsx
+++ client/src/components/users/Login.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="container">
@@ -8,9 +28,36 @@ const Login = () => {
         <div className="login-area">
           <h2>Welcome to YelpCamp by Tuhar Damahe</h2>
           <p>Please log-in to your account and start the adventure</p>
-          <input type="email" name="" id="" placeholder="Email" />
-          <input type="password" name="" id="" placeholder="Password" />
-          <button>Login</button>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                required
+                autoComplete="off"
+                value={user.email}
+                onChange={handleInput}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter your password"
+                required
+                autoComplete="off"
+                value={user.password}
+                onChange={handleInput}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+
           <p>
             New on our platform?
             <Link to="/register"> Create an account</Link>
